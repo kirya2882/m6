4fa13b4dab555d2ce98b7d831c98ce03a635106a
kirya2882 <podolskijk1@gmail.com>
2025-06-07 18:06:43 +0200

    site

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,13 +1,68 @@
 <script setup>
+import { ref } from "vue";
 
+let name = ref("");
+let src = ref("");
+let price = ref("");
+let desc = ref("")
+function sendItem(){
+    let data = {
+        name: name.value,
+        img: src.value,
+        price: price.value,
+        desc: desc.value
+    };
+    fetch("https://3708b522464b7037.mokky.dev/products", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+}
 </script>
 
+
+
 <template>
-    <section>
-        ОТВЕТЫ
-    </section>
+  <form @submit.prevent="sendItem">
+    <label>
+        <p>Введите название</p>
+        <input v-model="name" required />
+        {{ name }}
+    </label>
+    <label>
+      <p>Введите картинку:</p>
+      <input v-model="src" required />
+      <img :src="src" v-if="src"/>
+    </label>
+    <label>
+        <p>Введите цену</p>
+        <input v-model="price" type="number" required>
+    </label>
+    <label>
+        <p>Введите описание</p>
+        <input v-model="desc" required>
+    </label>
+    <button type="submit">Отправить</button>
+  </form>
 </template>
 
 <style scoped>
-
+    form{
+        margin: 10% 20%;
+        width: 60%;
+        padding: 2%;
+        border-radius: 25px;
+        background-color: rgba(35, 31, 35, 0.533);
+    }
+    input{
+        width:100%;
+    }
+    label{
+        margin: 2% 0;
+        display: block;
+    }
+    p{
+        color: white;
+        font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -10,6 +10,28 @@ async function getData(){
 onMounted(async ()=>{
     await getData()
 })
+let sort = ref('up')
+function sortData(){
+  if (sort.value == "up"){
+      data.value.sort((a,b)=>{
+    return a.price - b.price;
+    });
+  }
+  else if (sort.value == 'down'){
+  data.value.sort((a,b)=>{
+    return b.price - a.price;
+    });
+  }
+  
+}
+let search = ref("")
+function searchData(){
+  search.value = search.value.trim().toLowerCase()
+  data.value = data.value.filter((item)=>{
+    return item.name.toLowerCase().includes(search.value) || item.desc.toLowerCase().includes(search.value)
+  });
+}
+
 // let data = [
 //     {
 //         img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQroncSo8XhvWyQgIG8v4Tn0q6doB0oD3qqDXFz-jgmSLmhgKiC_J7rIHrIR7go9qh_tyA&usqp=CAU',
@@ -42,11 +64,27 @@ onMounted(async ()=>{
 </script>
 <template>
   <section>
-    <Card v-for="(item,index) in data" :key="index" :item="item"/>
+    <nav>
+      <label>
+        <p>соортировка</p>
+        <select v-model="sort" @change="sortData">
+          <option value="up">По возрастанию</option>
+          <option value="down">По убыванию</option>
+        </select>
+      </label>
+      <label>
+        <p>Поиск:</p>
+        <input v-model="search">
+        <button @click="searchData">Поиск</button>
+      </label>
+    </nav>
+    <div>
+      <Card v-for="(item,index) in data" :key="index" :item="item"/>
+    </div>
   </section>
 </template>
 <style scoped>
-    section{
+    div{
         background-color: rgb(55, 55, 55);
         display: grid;
         grid-template-columns: 1fr 1fr 1fr;
